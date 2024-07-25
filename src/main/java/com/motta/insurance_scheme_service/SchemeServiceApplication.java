8e080206b80a712a64e947047da94469eae8832e
arunpt7777@gmail.com <arunpt7777@gmail.com>
2024-07-25 12:33:58 +0530

    Added CORS policy exception setting

--- src/main/java/com/motta/insurance_scheme_service/SchemeServiceApplication.java
+++ src/main/java/com/motta/insurance_scheme_service/SchemeServiceApplication.java
@@ -2,12 +2,25 @@ package com.motta.insurance_scheme_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SchemeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchemeServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("http://localhost:3000");
+			}
+		};
 	}
 
 }
